5aad251c23fc5600e8a8d46183f945ba95576ba4
SUKANYA NAPHIJAI <142479565+hi-007@users.noreply.github.com>
2025-08-25 15:34:54 +0700

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,13 +7,23 @@
 //   base: "/sme-demo/"
 // });
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   //base: '/sme-demo/',   // ✅ ใส่เฉพาะชื่อ repo
+//   build: {
+//     chunkSizeWarningLimit: 1000,  // ตั้งขนาด chunk ให้มากกว่า 500 KB เช่น 1000 KB หรือ 1 MB
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  //base: '/sme-demo/',   // ✅ ใส่เฉพาะชื่อ repo
   build: {
-    chunkSizeWarningLimit: 1000,  // ตั้งขนาด chunk ให้มากกว่า 500 KB เช่น 1000 KB หรือ 1 MB
+    chunkSizeWarningLimit: 1000
   }
 })
